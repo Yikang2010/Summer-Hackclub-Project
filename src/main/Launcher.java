fcdeb28815e02c01afca32204c4fd3dcce2cf80e
yikia <yikia@Phoenixkingpromax>
2026-06-25 13:59:29 -0700

    Commit : main screen!

--- src/main/Launcher.java
+++ src/main/Launcher.java
@@ -1,19 +1,18 @@
 package main;
 
 import game_engine.GameManager;
-import _2D_Shooter.Frame; // Changed to Frame
+import main_screen.MainScreen;
+import _2D_Shooter.Frame;
 import chess.Game;
 import soccerMiniGame.SoccerManager;
 
 public class Launcher {
     public static void main(String[] args) {
         GameManager boss = new GameManager();
-        // Add games in exact order
-        boss.addGame(new Frame(boss));          
-        boss.addGame(new Game(boss));          
-        boss.addGame(new SoccerManager(boss)); 
-
-        // Start the engine
-        boss.startEngine();
+        boss.addGame(new MainScreen(boss));
+        boss.addGame(new Frame(boss));
+        boss.addGame(new Game(boss));
+        boss.addGame(new SoccerManager(boss));
+        boss.startEngine(); 
     }
 }
